0b4c9f1dc248edd1e25e35b3c46fbe66b731fc1b
Nexa Tech <kgfrocky7864@gmail.com>
2026-03-24 11:21:47 +0530

    Create practice2.py

---
+++ day 2/practice2.py
@@ -0,0 +1,27 @@
+# 1. crating variables my name,age and city. and print them all
+
+name = "Raaz"
+age = 17
+city = "Patna"
+
+print(name)
+print(age)
+print(city)
+
+ # 2. creaing a variable is_employed = false and printing it.
+
+is_employed = False  #boolean
+
+print(is_employed)
+ 
+# 3. reassigning a variable from a number to a sting and printing both 
+
+x = 10
+print("Before:", x)
+
+x = "raaz" 
+print("After:",x)
+ 
+print(type(name))
+print(type(age))
+print(type(is_employed))
