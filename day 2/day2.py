787314296c45afd59014e53af128dfa26b0a4904
Nexa Tech <kgfrocky7864@gmail.com>
2026-03-24 11:20:54 +0530

    Create day2.py

---
+++ day 2/day2.py
@@ -0,0 +1,22 @@
+name = "Raaz"      # string
+age = 17           #integar
+gpa = 22.5         #float
+is_student = True
+
+
+print(name)
+print(age)
+print(gpa)
+print(is_student)
+
+#checking variables types
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+#changing age (variable) value
+
+age = 18
+
+print(age)
