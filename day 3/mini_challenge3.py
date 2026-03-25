e5abf7d0eb0bfdb3f836b3494fab1f578181e49f
Nexa Tech <kgfrocky7864@gmail.com>
2026-03-25 19:27:55 +0530

    Add files via upload

---
+++ day 3/mini_challenge3.py
@@ -0,0 +1,4 @@
+#Write a program that takes a full name like "john doe" and converts it to "John Doe" using string methods.
+full_name = "john doe"
+formatted_name = full_name.title()  
+print(formatted_name)  # Output: John Doe
