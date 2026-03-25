88250f9078c838bd9aab9c148206e03bb1196b82
Nexa Tech <kgfrocky7864@gmail.com>
2026-03-25 19:21:27 +0530

    Add files via upload

---
+++ day 1/mini_challenge1.py
@@ -0,0 +1,6 @@
+#creating a file called mini_challenge1.py that print 5 fact about python programming language
+#1. Python was created by Guido van Rossum and first released in 1991.
+#2. Python is an interpreted language, which means that it does not need to be compiled before running.
+#3. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming   styles.
+#4. Python has a large standard library that provides many useful modules and functions for various tasks, such as file handling, regular expressions, and web development.             
+#5. Python is widely used in various fields, including web development, data science, machine learning, artificial intelligence, and scientific computing.
