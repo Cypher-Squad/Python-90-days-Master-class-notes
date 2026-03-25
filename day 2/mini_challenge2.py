2e43453bf928261cd03419871e1eb3bb4cc4bf73
Nexa Tech <kgfrocky7864@gmail.com>
2026-03-25 19:27:31 +0530

    Add files via upload

---
+++ day 2/mini_challenge2.py
@@ -0,0 +1,14 @@
+#Create a "student profile" with 6 variables (name, age, course, gpa, city, is_active) and print them with labels.
+name = "John Doe"
+age = 20
+course = "Computer Science"
+gpa = 3.5
+city = "New York"
+is_active = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Course: {course}")
+print(f"GPA: {gpa}")
+print(f"City: {city}")
+print(f"Is Active: {is_active}")
